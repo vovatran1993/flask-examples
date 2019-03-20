791a601568996359873c8a956b424296cf9fec6b
vovatran1993 <vovatran1993@gmail.com>
2019-03-20 18:49:16 -0500

    variable

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,19 +2,19 @@ node {
 properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give to build IP', name: 'IP', trim: true)])])
    
     stage("Install git"){
-        sh 'ssh    ec2-user@IP   sudo yum install git -y'
+        sh "ssh    ec2-user@${IP}   sudo yum install git -y"
     }
     stage("Clone a repo"){
         git 'https://github.com/farrukh90/flask-examples.git'
     }
     stage("Copy files"){
-        sh "scp * ec2-user@IP:/tmp/"
+        sh "scp * ec2-user@${IP}:/tmp/"
     }
     stage("Install requirements"){
-        sh "ssh ec2-user@IP   sudo pip install -r /tmp/requirements.txt"
+        sh "ssh ec2-user@${IP}   sudo pip install -r /tmp/requirements.txt"
     }
     stage("Run App"){
-        sh 'ssh ec2-user@IP  python  /tmp/01-hello-world/hello.py'
+        sh "ssh ec2-user@${IP}  python  /tmp/01-hello-world/hello.py"
     }
 
 }

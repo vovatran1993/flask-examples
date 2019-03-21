5685bab811262d189e6e6d9cd2a9b75b9085d737
vovatran1993 <vovatran1993@gmail.com>
2019-03-20 19:01:10 -0500

    d

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ node {
 properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give to build IP', name: 'IP', trim: true)])])
    
     stage("Install git"){
-        sh "ssh    ec2-user@${IP}   sudo yum install git -y"
+        sh "ssh    ec2-user@${IP}   sudo yum install git python-pip -y"
     }
     stage("Clone a repo"){
         git 'git@github.com:vovatran1993/flask-examples.git'
